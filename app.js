e106b93d3657d409a35b4aff3398cc466baf8e88
fmasson798 <FayeMasson@gmail.com>
2023-07-14 20:29:42 +0100

    db & user profile added
    Attempted to get database connected & edited user profile page with html & css - to be picked up here

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const path = require('path');
 let app = express();
+let read;
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+
+// connecting to PhpMyAdmin Database
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '',
+    database: '40200272',
+    port: '3306',
+    multipleStatements: true
+});
+
+// connect to database
+connection.connect((err)=>{
+    if(err) return console.log(err.message);
+    console.log("connected to local mysql db");
+});
 
 // route from localhost:3000 to home landing page
 app.get("/", (req, res) => {
@@ -36,10 +55,34 @@ app.get("/views/home.ejs", (req, res) => {
     res.render('sign_up');
 });
 
+//  //route for User button in footer
+//  app.get("/views/user.ejs", (req, res) => {
+//     let read_data_from_db = ` SELECT * FROM user `;
+//     connection.query( read, (err, userdbdata) => {
+//         if(err) throw err;
+//         res.render('user', {userdbdata});
+//     });
+// });
+
  //route for User button in footer
  app.get("/views/user.ejs", (req, res) => {
-    res.render('user');
-});
+        res.render('user');
+    });
+
+// /************************ 
+//   Testing User Data from Database
+//   **************************/
+
+// app.get('/user',  (req, res) => {
+//     let read = ` SELECT * FROM user `;
+//     connection.query( read, (err, userdata) => {
+//         if(err) throw err;
+//         res.render('user', {userdata});
+//     });
+// });
+
+
+
 
  //route for card in footer - TO BE REMOVED
  app.get("/views/card.ejs", (req, res) => {
@@ -57,9 +100,14 @@ app.get("/views/many_deals.ejs", (req, res) => {
     res.render('many_deals')
 });
 
+/************************ 
+  Testing Database
+  **************************/
 
-
-
+// app.get('/user', (req, res) => {
+//     let title = "User Table from Database";
+//     let read_user_data = "SELECT * FROM user";
+// });
 
 
 
